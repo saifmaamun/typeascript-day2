e34aa0ea70017a443cc557bddfdb69e662662a90
Saif Maamun <sadifmamun@gmail.com>
2023-05-02 22:04:51 +0600

    interface vs type

--- src/interface.ts
+++ src/interface.ts
@@ -20,3 +20,26 @@ const manInt: IUser={
     age:10
 }
 
+
+// can extend with interface
+interface IExtendUser extends IUser{
+    position:string,
+}
+
+const eManInt: IExtendUser={
+    name:'extend',
+    age:10,
+    position:'high'
+}
+
+// can extend with type
+
+type TExtendUser= TUser &{
+    designation:string
+}
+
+const eManTyp: TExtendUser= {
+    name:'extend',
+    age:10,
+    designation:'high'
+}
