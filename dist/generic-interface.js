8a395aff821d8be941f9406077437a98341d85ab
Saif Maamun <sadifmamun@gmail.com>
2023-05-03 14:59:09 +0600

    generic interface

---
+++ dist/generic-interface.js
@@ -0,0 +1,19 @@
+"use strict";
+const editaInfo = {
+    family: {
+        name: 'Edita',
+        fathersName: 'janina',
+        mothersName: 'janina',
+        addresses: 'kandapara',
+    },
+    education: {
+        class: 9,
+        group: 'science',
+        school: 'prottasaha',
+    },
+    finance: {
+        resident: 'own',
+        tutionFee: 0,
+        admissionFee: 0,
+    }
+};
---
+++ src/generic-interface.ts
@@ -0,0 +1,52 @@
+
+interface FamilyInterface{
+    name: string;
+    fathersName:string;
+    mothersName:string;
+    addresses:string;
+}
+interface EducationInterface{
+    class:number;
+    group?:string;
+    school?:string;
+    collage?:string;
+}
+
+interface FinanceInterface{
+    resident:string;
+    fathersSalary?:number;
+    tutionFee:number;
+    admissionFee:number;
+}
+
+type Info<T>=Array<T>;
+
+interface Student<X,Y,Z>{
+    family:X,
+    education:Y,
+    finance:Z,
+}
+
+
+const editaInfo:Student<FamilyInterface,EducationInterface,FinanceInterface>={
+    family:{
+        name: 'Edita',
+        fathersName:'janina',
+        mothersName:'janina',
+        addresses:'kandapara',
+    },
+    education:{
+        class:9,
+        group:'science',
+        school:'prottasaha',
+    },
+    finance:{
+        resident:'own',
+        tutionFee:0,
+        admissionFee:0,
+    }
+}
+
+
+
+
